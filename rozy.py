ae101dfe24da75cb99c540049c458d3ee764ffe3
dameli <95752876+damelinauryzbay@users.noreply.github.com>
2022-03-29 19:07:06 +0600

    Update rozy.py

--- rozy.py
+++ rozy.py
@@ -1,22 +1,43 @@
 from flask import Flask
 from flask import render_template
 from data import db_session
-import requests
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField, TextAreaField, SubmitField, EmailField
+from wtforms.validators import DataRequired
+from data.db_session import SqlAlchemyBase
+import sqlalchemy
+
 # import sqlalchemy
 app = Flask(__name__)
-colors = ['red', 'blue']
+colors = [('blue', 'white'), ('white', 'red'), ('yellow', 'orange')]
 number_of_roses = 0
 API_KEY = '40d1649f-0493-4b70-98ba-98533de7710b'
 
 
 class ShopImage():
     def show_map(self, address):
-        self.map_request = f"http://static-maps.yandex.ru/1.x/?ll=52.268104,76.947450&l=map"
+        map_request = f"http://static-maps.yandex.ru/1.x/?ll=76.947450,52.268104&z=18.98"
+
+
+#
+# class User(SqlAlchemyBase):
+#     __tablename__ = 'users'
+#
+#     email = sqlalchemy.Column(sqlalchemy.String,
+#                               index=True, unique=True, nullable=True)
+#     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+#
+
+class RegisterForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
+    submit = SubmitField('Войти')
 
 
 @app.route('/')
-@app.route('/rozy')
 def index():
+    # iage = adds
     return render_template("design.html")
 
 
@@ -32,6 +53,7 @@ def roses():
     name_of_photo = '/static/roses.jpg'
     return render_template("flowers.html",
                            title=title, head=head, price=price, info=info, photo=name_of_photo, name=name)
+
 
 @app.route('/chrys')
 def chrys():
@@ -54,9 +76,10 @@ def pions():
     price = 490
     info = 'Пионы гардения - прекрасные и пышные цветы так излюбленные многими людьми. Белые лепестки задорно ' \
            'искрятся в солнечных лучах благодаря нежному розовому отливу. Они впишутся в любой праздничный букет!'
-    name_of_photo = '/static/chrys.jpg'
+    name_of_photo = '/static/pions.jpg'
     return render_template("flowers.html",
                            title=title, head=head, price=price, info=info, photo=name_of_photo, name=name)
+
 
 @app.route('/gvozdiki')
 def gvoz():
@@ -66,29 +89,33 @@ def gvoz():
     price = 430
     info = 'Гвоздики Дон Педро - самые красные и популярные гвоздики. Являются символом любви и будут ' \
            'лучшим выбором для подарка любимому человеку!'
-    name_of_photo = '/static/gvozdiki.jpg'
+    name_of_photo = '/static/gvordiki.jpg'
     return render_template("flowers.html",
                            title=title, head=head, price=price, info=info, photo=name_of_photo, name=name)
 
-#
-# def main():
-#     db_session.global_init("db/blogs.db")
-#     app.run()
 
-#
-# class Order:
-#     __tablename__ = "orders"
-#     name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-#     email = sqlalchemy.Column(sqlalchemy.String,
-#                               index=True, unique=True, nullable=True)
-#     address = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-#     phonenumber = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
-#     numberofroses = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
-#
-#
-# order = Order()
-# number_of_roses += order.numberofroses
+@app.route('/wrapper')
+def wrapper():
+    return render_template("wrappers.html")
 
+
+def main():
+    db_session.global_init("db/blogs.db")
+    app.run()
+
+
+class Order:
+    __tablename__ = "orders"
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String,
+                              index=True, unique=True, nullable=True)
+    address = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    phonenumber = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    numberofroses = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+
+
+order = Order()
+number_of_roses += order.numberofroses
 
 if __name__ == '__main__':
     app.run()
