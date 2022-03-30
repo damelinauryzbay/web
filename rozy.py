464af39bf960dca22a767bf69ba6ab404b6df337
dameli <95752876+damelinauryzbay@users.noreply.github.com>
2022-03-30 21:58:42 +0600

    Update rozy.py

--- rozy.py
+++ rozy.py
@@ -1,13 +1,9 @@
-from flask import Flask
+from flask import Flask, redirect
 from flask import render_template
 from data import db_session
-from flask_wtf import FlaskForm
-import requests
-from wtforms import PasswordField, StringField, TextAreaField, SubmitField, EmailField
-from wtforms.validators import DataRequired
 from data.db_session import SqlAlchemyBase
 import sqlalchemy
-import shutil
+from user import RegisterForm
 
 # import sqlalchemy
 app = Flask(__name__)
@@ -16,37 +12,42 @@ number_of_roses = 0
 API_KEY = '40d1649f-0493-4b70-98ba-98533de7710b'
 
 
-# class ShopImage():
-#     def show_map(self, address):
-#         map_request = f"http://static-maps.yandex.ru/1.x/?ll=76.947450,52.268104&z=5"
+class User(SqlAlchemyBase):
+    __tablename__ = 'users'
+
+    email = sqlalchemy.Column(sqlalchemy.String,
+                              index=True, unique=True, nullable=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
 
 
-#
-# class User(SqlAlchemyBase):
-#     __tablename__ = 'users'
-#
-#     email = sqlalchemy.Column(sqlalchemy.String,
-#                               index=True, unique=True, nullable=True)
-#     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-#
-
-class RegisterForm(FlaskForm):
-    email = EmailField('Почта', validators=[DataRequired()])
-    password = PasswordField('Пароль', validators=[DataRequired()])
-    password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
-    submit = SubmitField('Войти')
-
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
 
 @app.route('/')
 def index():
-    map_request = f"http://static-maps.yandex.ru/1.x/?ll=76.947450,52.268104&z=15&l=map"
-    response = requests.get(map_request)
-    map_file = "map.png"
-    with open(map_file, "wb") as file:
-        file.write(response.content)
-    shutil.move('C:/Пользователи/User/PycharmProjects/pythonProject1/map.png',
-                'C:/Пользователи/User/PycharmProjects/pythonProject1/static')
-    return render_template("design.html", image='/static/map.png')
+    # iage = adds
+    return render_template("design.html")
 
 
 @app.route('/roses')
