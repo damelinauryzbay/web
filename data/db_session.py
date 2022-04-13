1430c7c87b7afcef9c0944889e33120423269095
Zhamilya <94537577+Zhamilekin@users.noreply.github.com>
2022-04-13 18:17:44 +0600

    Update db_session.py

--- data/db_session.py
+++ data/db_session.py
@@ -7,6 +7,7 @@ SqlAlchemyBase = dec.declarative_base()
 
 __factory = None
 
+
 def global_init(db_file):
     global __factory
 
@@ -26,3 +27,7 @@ def global_init(db_file):
 
     SqlAlchemyBase.metadata.create_all(engine)
 
+
+def create_session() -> Session:
+    global __factory
+    return __factory()
